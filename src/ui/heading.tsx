804a8ac304b42a0ea17f19dbc2f0d26f8fb5e603
ackwell <saxon@ackwell.com.au>
2021-03-13 00:49:38 +1100

    Add accessible label to heading

--- src/ui/heading.tsx
+++ src/ui/heading.tsx
@@ -1,10 +1,29 @@
-import {ReactNode} from 'react'
+import {ReactNode, useEffect, useRef, useState} from 'react'
 import styles from './heading.module.css'
 
 export interface HeadingProps {
 	children?: ReactNode
+	id?: string
 }
 
-export function Heading({children}: HeadingProps) {
-	return <h2 className={styles.heading}>{children}</h2>
+export function Heading({children, id}: HeadingProps) {
+	// Headings have a `text-transform: uppercase` on them, which reads poorly in ARIA.
+	// Extract the text content of children and set it on the label prop to improve.
+	const [childText, setChildText] = useState('')
+
+	const headingRef = useRef<HTMLHeadingElement>(null)
+	useEffect(() => {
+		setChildText(headingRef.current?.textContent ?? '')
+	}, [children])
+
+	return (
+		<h2
+			ref={headingRef}
+			id={id}
+			className={styles.heading}
+			aria-label={childText}
+		>
+			{children}
+		</h2>
+	)
 }
