cec3345ecd401e9df6c11b1e3a6273c18111274f
ackwell <saxon@ackwell.com.au>
2021-03-16 16:07:51 +1100

    Quick'n'dirty pull marker palette item

--- src/palette/item.tsx
+++ src/palette/item.tsx
@@ -2,26 +2,30 @@ import Tooltip from '@reach/tooltip'
 import {TooltipDetail} from '@xivanalysis/tooltips'
 import {forwardRef, ReactNode} from 'react'
 import {useActionData} from '../data'
-import {ActionItem, Item, ItemType} from '../state'
+import {ActionItem, Item, ItemType, PullItem} from '../state'
 import {ActionIcon} from '../ui'
 import styles from './item.module.css'
 
 import '@reach/tooltip/styles.css'
 
-export interface PaletteItemViewProps {
-	item: Item
+interface ItemViewProps<I extends Item> {
+	item: I
 }
+
+export type PaletteItemViewProps = ItemViewProps<Item>
 
 export function PaletteItemView({item}: PaletteItemViewProps) {
 	switch (item.type) {
 		case ItemType.ACTION:
 			return <ActionItemView item={item} />
+		case ItemType.PULL:
+			return <PullItemView item={item} />
 		default:
-			return <div>UNK:{ItemType[item.type]}</div>
+			return <div>UNK:{ItemType[(item as any).type]}</div>
 	}
 }
 
-export function ActionItemView({item}: {item: ActionItem}) {
+function ActionItemView({item}: ItemViewProps<ActionItem>) {
 	const action = useActionData(item.action)
 	return (
 		<Tooltip
@@ -35,6 +39,15 @@ export function ActionItemView({item}: {item: ActionItem}) {
 				/>
 			</div>
 		</Tooltip>
+	)
+}
+
+function PullItemView({item}: ItemViewProps<PullItem>) {
+	return (
+		<PaletteInfo
+			icon={<div className={styles.pullIcon}>Pull</div>}
+			name="Pull marker"
+		/>
 	)
 }
 
